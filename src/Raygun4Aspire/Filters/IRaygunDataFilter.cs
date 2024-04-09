39191ee6defcc6328cbf0279c6bf7926e909029d
QuantumNightmare <quantumnightmare@hotmail.com>
2024-04-10 11:19:03 +1200

    Fixed up some xml code comments

--- src/Raygun4Aspire/Filters/IRaygunDataFilter.cs
+++ src/Raygun4Aspire/Filters/IRaygunDataFilter.cs
@@ -3,18 +3,18 @@
   public interface IRaygunDataFilter
   {
     /// <summary>
-    /// Returns whether or not this filter will be able to parse the following data.
-    /// This method is called to determine if the following Filter method will be called.
+    /// Returns whether or not this filter will be able to parse the given data.
+    /// This method is called to determine if the subsequent Filter method will be called.
     /// </summary>
-    /// <returns><c>true</c>, if the data is parsable by the implemented class, <c>false</c> otherwise.</returns>
-    /// <param name="data">Data.</param>
+    /// <returns><c>true</c>, if the data is can be parsed by the implemented class, <c>false</c> otherwise.</returns>
+    /// <param name="data">The raw request data payload.</param>
     bool CanParse(string data);
 
     /// <summary>
-    /// Filter the specified data by checking for the following keys whose values will be removed.
+    /// Filter the specified data by checking for the given keys whose values will be removed.
     /// </summary>
-    /// <returns>The filter.</returns>
-    /// <param name="data">Data.</param>
+    /// <returns>The filtered raw request data payload.</returns>
+    /// <param name="data">The raw request data payload.</param>
     /// <param name="ignoredKeys">Keys whose values should be removed.</param>
     string Filter(string data, IList<string> ignoredKeys);
   }
