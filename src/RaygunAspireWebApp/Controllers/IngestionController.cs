ac44ba66de15d3a09d93e9e3bea9a862a3f3d1bb
QuantumNightmare <quantumnightmare@hotmail.com>
2024-04-10 15:20:47 +1200

    Removed temporary debug logging from the Raygun portal app

--- src/RaygunAspireWebApp/Controllers/IngestionController.cs
+++ src/RaygunAspireWebApp/Controllers/IngestionController.cs
@@ -4,36 +4,33 @@ using System.Text.Json;
 
 namespace RaygunAspireWebApp.Controllers
 {
-    public class IngestionController : Controller
+  public class IngestionController : Controller
+  {
+    public async Task<IActionResult> Entries()
     {
-        public async Task<IActionResult> Entries()
+      try
+      {
+        using var reader = new StreamReader(Request.Body, Encoding.UTF8);
+        string requestBody = await reader.ReadToEndAsync();
+
+        var raygunMessage = JsonSerializer.Deserialize<Mindscape.Raygun4Net.RaygunMessage>(requestBody);
+
+        if (raygunMessage != null)
         {
-            Console.WriteLine("Yayy, received a Raygun crash report");
+          var info = Directory.CreateDirectory("/app/raygun/errors");
 
-            try
-            {
-                using var reader = new StreamReader(Request.Body, Encoding.UTF8);
-                string requestBody = await reader.ReadToEndAsync();
-                Console.WriteLine(requestBody);
+          var message = raygunMessage.Details.Error.Message;
+          var uniqueSlug = DateTime.UtcNow.Ticks;
 
-                var raygunMessage = JsonSerializer.Deserialize<Mindscape.Raygun4Net.RaygunMessage>(requestBody);
-
-                var info = Directory.CreateDirectory("/app/raygun/errors");
-
-                Console.WriteLine("FULL FILE PATH:");
-                Console.WriteLine(info.FullName);
-
-                var message = raygunMessage.Details.Error.Message;
-                var uniqueSlug = DateTime.UtcNow.Ticks;
-
-                System.IO.File.WriteAllText($"/app/raygun/errors/{uniqueSlug}|{message}.json", requestBody);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-
-            return Accepted();
+          System.IO.File.WriteAllText($"/app/raygun/errors/{uniqueSlug}|{message}.json", requestBody);
         }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.ToString());
+      }
+
+      return Accepted();
     }
+  }
 }
