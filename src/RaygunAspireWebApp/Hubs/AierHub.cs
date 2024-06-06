cf0b84b4c7e9cdc23131e9bb19fbba9d35c6bf3e
QuantumNightmare <quantumnightmare@hotmail.com>
2024-06-06 16:34:17 +1200

    Delete unused method

--- src/RaygunAspireWebApp/Hubs/AierHub.cs
+++ src/RaygunAspireWebApp/Hubs/AierHub.cs
@@ -4,9 +4,5 @@ namespace RaygunAspireWebApp.Hubs
 {
   public class AierHub : Hub
   {
-    public async Task AppendText(string text)
-    {
-      await Clients.All.SendAsync("ReceiveText", text);
-    }
   }
 }
