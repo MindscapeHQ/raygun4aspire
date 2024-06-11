c80ca78d1fde2478f56dd99c0f71af50322c22f3
QuantumNightmare <quantumnightmare@hotmail.com>
2024-06-11 16:13:31 +1200

    Delete the Raygun app from this repo as it's been moved to Aspire.Hosting.Raygun

--- src/RaygunAspireWebApp/Models/ErrorListViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RaygunAspireWebApp.Models
-{
-  public class ErrorListViewModel
-  {
-    public List<ErrorInstanceRow> Errors { get; set; }
-    public int Loaded { get; set; }
-    public int Total { get; set; }
-  }
-}
